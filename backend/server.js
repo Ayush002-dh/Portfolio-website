9a443551b37a525d9d555eae4f0cd6d7874ccc3c
Ayush <aayushdhiman9899@gmail.com>
2026-08-04 14:43:11 +0530

    Fix backend repository

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors({
-    origin: "https://ayush002-dh.github.io"
-}));
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
